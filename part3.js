9fb296fdb7c48c810dcb4b4cefcddf17f2113251
Stephen Gadd <stephen_gadd@homedepot.com>
2016-05-30 14:23:10 -0400

    first commit

---
+++ part3.js
@@ -0,0 +1,10 @@
+/**
+ * Created by stephengadd on 5/30/16.
+ */
+"use strict";
+
+console.log(`1 + 1 =  ${ 1 + 1 }`);
+
+let childe_reland = `I saw them and I knew them all. And yet Dauntless the
+slug horn`;
+
